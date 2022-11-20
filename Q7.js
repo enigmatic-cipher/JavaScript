104d976d85cf952f06e1175dde4f16993fd4c1ba
Amit <enigmatic.cipher@gmail.com>
2022-11-20 12:46:20 +0530

    js

---
+++ Q7.js
@@ -0,0 +1,10 @@
+// Return the First Element in an Array
+
+const firstElement = (a) => {
+  let z = a[0];
+  return z;
+};
+
+let x = [1, 2, 3, 4, 5];
+let y = firstElement(x);
+console.log(`First element of array is ${y}`);
