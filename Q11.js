8e22f186d395debf5b41263cea01b51ba7aca3c7
Amit <enigmatic.cipher@gmail.com>
2022-11-20 12:59:37 +0530

    js

---
+++ Q11.js
@@ -0,0 +1,10 @@
+// Return remainder of two number.
+
+const remainder = (num1, num2) => {
+  return num1 % num2;
+};
+
+let n1 = 7;
+let n2 = 2;
+let x = remainder(n1, n2);
+console.log(`Remainder of Number = ${x}`);
